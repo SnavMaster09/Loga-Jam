ce4fd8e0c072b4b6f1fd642512fa6ff7e82cef6b
SnavMaster09 <octav.work.dev@gmail.com>
2023-05-20 19:40:10 +0300

    Scene Added

--- Assets/Scripts/Enemies/Enemy_Spawner.cs
+++ Assets/Scripts/Enemies/Enemy_Spawner.cs
@@ -15,7 +15,7 @@ public class Enemy_Spawner : MonoBehaviour
         int numEnemies = Random.Range(1, maxEnemies);
         for(int i=1;i<=numEnemies;i++)
         {
-            Instantiate(spider, new Vector3(gameObject.transform.position.x + Random.Range(-4.5f, 4.5f), gameObject.transform.position.y + Random.Range(-4.5f, 4.5f), 0), transform.rotation);
+            Instantiate(spider, new Vector3(gameObject.transform.position.x + Random.Range(-4.5f, 4.5f), gameObject.transform.position.y + Random.Range(-3f, 4.5f), 0), transform.rotation);
         }
         
     }
